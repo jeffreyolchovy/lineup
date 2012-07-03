bfe08e98d3baf786d72df7ad73924be427997840
Jeffrey Olchovy <olchovy@gmail.com>
2012-07-03 10:05:02 -0400

    DSL expects EOL

--- src/main/scala/com/olchovy/dsl/FitnessSpecificationParser.scala
+++ src/main/scala/com/olchovy/dsl/FitnessSpecificationParser.scala
@@ -9,37 +9,33 @@ object FitnessSpecificationParser extends RegexParsers
 {
   private case class Token(sign: String, statistic: Statistic.Value)
 
-  private lazy val token = sign ~ statistic ^^ { case sign ~ statistic ⇒ Token(sign, statistic) }
+  private lazy val EOL = """\z""".r
 
   private lazy val sign = ( "+" | "-" )
 
-  private lazy val statistic = """\w+""".r ^? ({
+  private lazy val statistic = """[\w|/]+""".r ^? ({
     case str if Statistic.values.exists(abbr ⇒ abbr.toString == str) ⇒ Statistic.withName(str)
   }, "Unknown statistic abbreviation: " + _)
 
-  lazy val specification: Parser[(Lineup, Player) => Double] = (token +) ^^ { tokens ⇒
-    val functions = tokens.map { case Token(sign, statistic) ⇒
-      (lineup: Lineup, player: Player) ⇒ sign match {
-        case "+" ⇒ player.stat(statistic) - lineup.stat(statistic)
-        case "-" ⇒ lineup.stat(statistic) - player.stat(statistic)
-      }
-    }
-
-    (lineup: Lineup, player: Player) ⇒ functions.map(_.apply(lineup, player)).sum
+  private lazy val token = sign ~ statistic ^^ {
+    case sign ~ statistic ⇒ Token(sign, statistic)
   }
 
-  def main(args: Array[String]) {
-    val attempt = args.mkString(" ")
+  private lazy val specification: Parser[(Lineup, Player) ⇒ Double] = (token +) ~ EOL ^^ {
+    case tokens ~ _ ⇒
+      val functions = tokens.map { case Token(sign, statistic) ⇒
+        (lineup: Lineup, player: Player) ⇒ sign match {
+          case "+" ⇒ player.stat(statistic) - lineup.stat(statistic)
+          case "-" ⇒ lineup.stat(statistic) - player.stat(statistic)
+        }
+      }
 
-    println("Attempting to parse: " + attempt)
+      (lineup: Lineup, player: Player) ⇒ functions.map(_.apply(lineup, player)).sum
+  }
 
-    parse(specification, attempt) match {
-      case Success(tree, _) ⇒
-        println(tree.toString)
-
-      case e: NoSuccess ⇒
-        println(e.toString)
-    }
+  def apply(input: String): (Lineup, Player) ⇒ Double = parse(specification, input) match {
+    case Success(result, _) ⇒ result
+    case e: NoSuccess ⇒ throw new RuntimeException(e.toString)
   }
 }
 
