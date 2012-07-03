129b0b922b15c319404a45a41028c008a82318a0
Jeffrey Olchovy <olchovy@gmail.com>
2012-07-03 11:16:59 -0400

    unit testing for domain, dsl, and io packages

---
+++ src/test/scala/com/olchovy/dsl/FitnessSpecificationParserSuite.scala
@@ -0,0 +1,51 @@
+package com.olchovy.dsl
+
+import org.scalatest.FunSuite
+
+
+class FitnessSpecificationParserSuite extends FunSuite
+{
+  private def parse(string: String) = FitnessSpecificationParser(string)
+
+  test("Parse valid string with one statistic") {
+    val string = "+OBA"
+    parse(string)
+  }
+
+  test("Parse valid string with multiple statistics") {
+    val string = "+OBA -PIP -2B"
+    parse(string)
+  }
+
+  test("Parse valid string with statistics that contain special characters") {
+    val string = "+H -HR/H +3B -OBA +AVG"
+    parse(string)
+  }
+
+  test("Parse valid string with no whitespace") {
+    val string = "+H-HR/H+3B-OBA+AVG"
+    parse(string)
+  }
+  
+  test("Parse invalid string") {
+    val string = "foo"
+    intercept[RuntimeException] {
+      parse(string)
+    }
+  }
+
+  test("Parse invalid string ending with valid string") {
+    val string = "foo +PIP"
+    intercept[RuntimeException] {
+      parse(string)
+    }
+  }
+
+  test("Parse invalid string beginning with valid string") {
+    val string = "-SLG bar"
+    intercept[RuntimeException] {
+      parse(string)
+    }
+  }
+}
+
